7afda6bbc7ed0794cedde3659f5ae1d978786f9b
Dominik <dominikjaniga91@gmail.com>
2020-03-28 17:18:25 +0100

    Add basic test for categories ones, twos, threes

--- src/main/java/yahtzee/Categories.java
+++ src/main/java/yahtzee/Categories.java
@@ -8,25 +8,37 @@ public enum Categories {
     ONES {
         @Override
         int getSumOfDice(Map<Integer, Integer> thrownDice) {
-            return thrownDice.values().stream().filter(number -> number==1).reduce(0,Integer::sum);
+            return thrownDice.values()
+                    .stream()
+                    .filter(number -> number==1)
+                    .reduce(0,Integer::sum);
         }
     },
     TWOS {
         @Override
         int getSumOfDice(Map<Integer, Integer> thrownDice) {
-            return thrownDice.values().stream().filter(number -> number==2).reduce(0,Integer::sum);
+            return thrownDice.values()
+                    .stream()
+                    .filter(number -> number==2)
+                    .reduce(0,Integer::sum);
         }
     },
     THREES {
         @Override
         int getSumOfDice(Map<Integer, Integer> thrownDice) {
-            return thrownDice.values().stream().filter(number -> number==3).reduce(0,Integer::sum);
+            return thrownDice.values()
+                    .stream()
+                    .filter(number -> number==3)
+                    .reduce(0,Integer::sum);
         }
     },
     FOURS {
         @Override
         int getSumOfDice(Map<Integer, Integer> thrownDice) {
-            return thrownDice.values().stream().filter(number -> number==4).reduce(0,Integer::sum);
+            return thrownDice.values()
+                    .stream()
+                    .filter(number -> number==4)
+                    .reduce(0,Integer::sum);
         }
     };
 
--- src/test/java/yahtzee/YahtzeeGameTest.java
+++ src/test/java/yahtzee/YahtzeeGameTest.java
@@ -23,9 +23,32 @@ public class YahtzeeGameTest {
     }
 
     @Test
+    @DisplayName("Result should be equals to 3 when player rolls 4 dice each 1 and 2 different ")
+    void shouldReturnResultEighteen_afterRolls3x1And2DifferentDice(){
+        Map<Integer, Integer> thrownDice = Map.of(1,1, 2,1, 3,1, 5,2, 6,1);
+        Assertions.assertThat(game.getScoreOfRound(thrownDice)).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("Result should be equals to 6 when player rolls 3 dice each 2 and 2 different ")
+    void shouldReturnResultEighteen_afterRolls3x2And2DifferentDice(){
+        Map<Integer, Integer> thrownDice = Map.of(1,2, 2,2, 3,2, 5,1, 6,1);
+        Assertions.assertThat(game.getScoreOfRound(thrownDice)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("Result should be equals to 9 when player rolls 3 dice each 3 and 2 different ")
+    void shouldReturnResultEighteen_afterRolls3x3And2DifferentDice(){
+        Map<Integer, Integer> thrownDice = Map.of(1,3, 2,3, 3,3, 5,2, 6,1);
+        Assertions.assertThat(game.getScoreOfRound(thrownDice)).isEqualTo(9);
+    }
+
+    @Test
     @DisplayName("Result should be equals to 12 when player rolls 3 dice each 4 and 2 different ")
     void shouldReturnResultTwelve_afterRolls3x4And2DifferentDice(){
         Map<Integer, Integer> thrownDice = Map.of(1,4, 2,4, 3,4, 5,2, 6,1);
         Assertions.assertThat(game.getScoreOfRound(thrownDice)).isEqualTo(12);
     }
+
+
 }
